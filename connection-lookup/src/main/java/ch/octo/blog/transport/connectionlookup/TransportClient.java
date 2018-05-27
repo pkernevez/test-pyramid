18e2c32a8fd9f1e7fb0d126e1f78837d0e68e994
JeromeVDL <jeromevdl@gmail.com>
2018-05-27 11:01:45 +0200

    better solution for fields array in feign client

--- connection-lookup/src/main/java/ch/octo/blog/transport/connectionlookup/TransportClient.java
+++ connection-lookup/src/main/java/ch/octo/blog/transport/connectionlookup/TransportClient.java
@@ -6,11 +6,19 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @FeignClient(value = "transport", url = "${transport.url}")
 public interface TransportClient {
 
-    @RequestMapping(method = GET, value = "/connections?fields[]=connections/from&fields[]=connections/to", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    Connections getConnections(@RequestParam("from") String from, @RequestParam("to") String to);
+    @RequestMapping(method = GET, value = "/connections", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    Connections getConnections(@RequestParam("from") String from, @RequestParam("to") String to, @RequestParam("fields[]") List<String> fields);
+
+    default Connections getConnections(String from, String to) {
+        return getConnections(from, to, Arrays.asList("connections/from", "connections/to"));
+    }
+
 }
